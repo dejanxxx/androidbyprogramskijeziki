056155957748860d26229f1a1124dc447322b6c5
dejanxxx <dejan.hrncic@gmail.com>
2011-04-11 14:50:46 +0200

    nov komit

--- src/com/test/MainActivity.java
+++ src/com/test/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        System.out.println("Test"); //by me
-        Toast.makeText(this, "test", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "test", Toast.LENGTH_LONG).show();  // dont spam my code
     }
 }
